45a3b4f67982fe73f9fdb3d4e07ce59b71a9bcb7
SZibaee <sjianz01@mail.bbk.ac.uk>
2018-12-16 17:33:46 +0100

    Add docstring. Add extra #start#s in tokenize(). Refactor make_pre_word_ngrams(). Delete _add_extra_start_tokens().

--- Main.py
+++ Main.py
@@ -4,56 +4,74 @@ from collections import Counter
 
 class Main(object):
 
-    # def __init__(self):
-    #     pass
-
-    def tokenize(self, corpus_tokens: str):
-        corpus_tokens = self.parse_punctuation(corpus_tokens)
-        corpus_tokens = corpus_tokens.lower()
-        corpus_tokens = corpus_tokens.split(' ')
-        corpus_tokens.insert(0, self.Strs.STRT.value)
+    def tokenize(self, corpus: str, n: int):
+        """
+        Convert a corpus to a list of lowercase words using white space as the delimiter, preserving the order of
+        words. Insert #start#(s) at start of sentences according to the size of the n-gram, replace punctuation that
+        terminates a sentence with #end#. Remove all other punctuation.
+        :param corpus: Space-delimited words.
+        :param n: Size of the n-gram (as number of words).
+        :return: List of tokens representing the input corpus, preserving word order.
+        """
+        corpus = self._parse_punctuation(corpus)
+        corpus = corpus.lower()
+        corpus_tokens = corpus.split(' ')
+        for i in range(n - 1):
+            corpus_tokens.insert(0, self.Strs.STRT.value)
         if corpus_tokens[len(corpus_tokens) - 1] != self.Strs.END.value:
             corpus_tokens.append(self.Strs.END.value)
         end_indices = [i for i, x in enumerate(corpus_tokens) if x == self.Strs.END.value and i != len(corpus_tokens)
                        - 1]
+        inserted_starts = 0
         for i, end_index in enumerate(end_indices):
-            corpus_tokens.insert(end_index + i + 1, self.Strs.STRT.value)
+            insert_start_index = end_index + 1
+            for j in range(n - 1):
+                corpus_tokens.insert(insert_start_index + inserted_starts, self.Strs.STRT.value)
+                inserted_starts += 1
         return corpus_tokens
 
-    def parse_punctuation(self, corpus: str):
+    def _parse_punctuation(self, corpus: str):
+        """
+        Removes any punctuation. Replaces punctuation that terminates a sentence with #end#.
+        :param corpus: Space-delimited words.
+        :return: Corpus with punctuation removed or replaced with #end#.
+        """
         pattern_nonstop_punct = re.compile(r'[][{},;:()/~"_|]+')
         pattern_stop_punct = re.compile('[.!?]+')
         corpus = pattern_nonstop_punct.sub('', corpus)
-        corpus = pattern_stop_punct.sub(self.Strs.END.value, corpus)
+        corpus = pattern_stop_punct.sub(' ' + self.Strs.END.value, corpus)
         return corpus
 
-    def count_occurences(self, corpus_tokens: str):
-        token_counts = Counter(corpus_tokens)
-        return token_counts
-
     def make_pre_word_ngrams(self, corpus_tokens: list, n: int):
-        corpus_tokens = self._add_extra_start_tokens(corpus_tokens, n)
-        pre_words_ngrams = [self.get_preceeding_words(corpus_tokens, i, n) for i in range(len(corpus_tokens))]
+        """
+        Make a list of n-gram words that precede each word, sampling the corpus with a sliding window of one word.
+        E.g. corpus_tokens=['#start#', 'i', 'like', 'python', 'programming', '#end#'] and n=3 would produce:
+        [['#start#','#start#'],['#start#','i'], ['i','like'],['like','python']]
+        :param corpus_tokens: A corpus stored as a list of words produced by self.tokenize(corpus_tokens).
+        :param n: Size of the n-gram (as number of words).
+        :return:
+        """
+        first_word_i = n - 1
+        end_i = len(corpus_tokens) - 1
+        pre_words_ngrams = [self._get_preceeding_words(corpus_tokens, i, n) for i in range(first_word_i, end_i)]
         return pre_words_ngrams
 
-    def get_preceeding_words(self, corpus_tokens: list, corpus_index: int, n: int):
+    def _get_preceeding_words(self, corpus_tokens: list, corpus_index: int, n: int):
         pre_words = []
-        if corpus_index - (n - 1) < 0:
-            for i in range(n - 1):
-                pre_words.append(self.Strs.STRT.value)
-        else:
+        if corpus_index - (n - 1) >= 0:
             for i in range(n - 1, 0, -1):
                 pre_words.append(corpus_tokens[corpus_index - i])
         return pre_words
 
-    def _add_extra_start_tokens(self, corpus_tokens: list, n: int):
-        if n < 2:
-            print('A bi-gram only requires 1 start token at beginning of corpus. No extra starts to be added.')
-        else:
-            num_of_extra_start_tokens_to_add = n - 2
-            for i in range(num_of_extra_start_tokens_to_add):
-                corpus_tokens.insert(0, self.Strs.STRT.value)
-        return corpus_tokens
+    def count_occurrences(self, corpus_tokens: str):
+        """
+        Count the number of occurrences of each word (token).
+        :param corpus_tokens: A corpus stored as a list of words produced by self.tokenize(corpus_tokens).
+        :return: Number of occurrences of a token as key-value pairs, with tokens as the key and number of
+        occurrences as the associated value)
+        """
+        token_counts = Counter(corpus_tokens)
+        return token_counts
 
     from enum import Enum
 
