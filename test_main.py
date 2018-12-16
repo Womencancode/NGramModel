105e28d490b5ae5d7c1b4aefdd548b394a7a9e70
SZibaee <sjianz01@mail.bbk.ac.uk>
2018-12-16 01:29:27 +0100

    Test 4 more methods.

--- test_main.py
+++ test_main.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 from Main import Main
+import sys
+from pympler.asizeof import asizeof
 
 
 class TestMain(TestCase):
@@ -7,6 +9,12 @@ class TestMain(TestCase):
     def setUp(self):
         self.main = Main()
         self.toy_corpus = 'I ,have|[] a :big h~()ouse! _The? "boat is light-blue.;'
+        self.toy_corpus_2 = 'There is a potato on my foot. There is foot on my potato. The potato that is on my foot ' \
+                            'has a foot on it.'
+        self.corpus_tokens_2 = ['#start#', 'there', 'is', 'a', 'potato', 'on', 'my', 'foot', '#end#', '#start#',
+                                'there', 'is', 'foot', 'on', 'my', 'potato', '#end#', '#start#', 'the', 'potato',
+                                'that', 'is', 'on', 'my', 'foot', 'has', 'a', 'foot', 'on', 'it', '#end#']
+        self.corpus_tokens_3 = ['#start#', 'i', 'like', 'python', 'programming', '#end#']
 
     def tearDown(self):
         self.main = None
@@ -23,3 +31,31 @@ class TestMain(TestCase):
         actual = self.main.parse_punctuation(self.toy_corpus)
         self.assertEqual(expected, actual)
 
+    def test_count_occurences(self):
+        expected = {'#start#': 3, 'there': 2, 'is': 3, 'a': 2, 'potato': 3, 'on': 4, 'my': 3, 'foot': 4, '#end#': 3,
+                    'the': 1, 'that': 1, 'has': 1, 'it': 1}
+        actual = self.main.count_occurences(self.corpus_tokens_2)
+        self.assertEqual(expected, actual)
+        print(str(asizeof(self.main, set())))
+
+    def test__add_extra_start_tokens(self):
+        expected = ['#start#', '#start#', '#start#', 'i', 'like', 'python', 'programming', '#end#']
+        actual = self.main._add_extra_start_tokens(self.corpus_tokens_3, 4)
+        self.assertEqual(expected, actual)
+
+    # def test_measure_sze_of_objects(self):
+    #     print('Size of main object: ' + str(sys.getsizeof(self.main, set())))
+    #     print('Deep size of main object: ' + str(asizeof(self.main, set())))
+
+    # def test_make_ngrams_indexed(self):
+
+    def test_get_preceeding_words(self):
+        expected = ['there', 'is', 'a']
+        actual = self.main.get_preceeding_words(self.corpus_tokens_2, 4, 4)
+        self.assertEqual(expected, actual)
+
+    def test_make_pre_word_ngrams(self):
+        expected = [['#start#', '#start#', '#start#'], ['#start#', '#start#', 'i'], ['#start#', 'i', 'like'],
+                    ['i', 'like', 'python']]
+        actual = self.main.make_pre_word_ngrams(self.corpus_tokens_3, 4)
+        self.assertEqual(expected, actual)
