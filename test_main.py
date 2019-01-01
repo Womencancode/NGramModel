36aa4d262d74d47f1a49f0b9c0caa40dd22d006b
SZibaee <sjianz01@mail.bbk.ac.uk>
2019-01-01 00:26:02 +0000

    Update modified variable names.

--- test_main.py
+++ test_main.py
@@ -42,13 +42,13 @@ class TestMain(TestCase):
 
     def test__make_ngrams_pre_word_True(self):
         expected = ('i', 'like', 'python')
-        actual = self.main._make_ngrams(corpus_tokens=self.corpus3_tokens_n4, corpus_index=6, n=4,
+        actual = self.main._make_ngrams(tokenized_corpus=self.corpus3_tokens_n4, corpus_token_index=6, n=4,
                                         make_pre_word_ngram=True)
         self.assertEqual(expected, actual)
 
     def test__make_ngrams_pre_word_False(self):
         expected = ('i', 'like', 'python', 'programming')
-        actual = self.main._make_ngrams(corpus_tokens=self.corpus3_tokens_n4, corpus_index=6, n=4,
+        actual = self.main._make_ngrams(tokenized_corpus=self.corpus3_tokens_n4, corpus_token_index=6, n=4,
                                         make_pre_word_ngram=False)
         self.assertEqual(expected, actual)
 
@@ -57,7 +57,7 @@ class TestMain(TestCase):
                     ('#start#', '#start#', 'i', 'like'): ('#start#', '#start#', 'i'),
                     ('#start#', 'i', 'like', 'python'): ('#start#', 'i', 'like'),
                     ('i', 'like', 'python', 'programming'): ('i', 'like', 'python')}
-        actual = self.main.make_ngrams_and_pre_word_ngrams(corpus_tokens=self.corpus3_tokens_n4, n=4)
+        actual = self.main.make_ngrams_and_pre_word_ngrams(tokenized_corpus=self.corpus3_tokens_n4, n=4)
         self.assertEqual(expected, actual)
 
     def test_make_ngrams_and_pre_word_ngrams_use_Lap_smooth(self):
@@ -65,7 +65,7 @@ class TestMain(TestCase):
                     ('#start#', '#start#', 'i', '#UNK#'): ('#start#', '#start#', 'i'),
                     ('#start#', 'i', 'like', '#UNK#'): ('#start#', 'i', 'like'),
                     ('i', 'like', 'python', '#UNK#'): ('i', 'like', 'python')}
-        actual = self.main.make_ngrams_and_pre_word_ngrams(corpus_tokens=self.corpus3_tokens_n4, n=4,
+        actual = self.main.make_ngrams_and_pre_word_ngrams(tokenized_corpus=self.corpus3_tokens_n4, n=4,
                                                            use_Lap_smooth=True)
         self.assertEqual(expected, actual)
 
@@ -91,6 +91,7 @@ class TestMain(TestCase):
         (sum(a|has) = 1 / sum(has) = 1) = 1
         (sum(foot|a) = 1 / sum(a) = 2) = 1/2
         (sum(it|on) = 1 / sum(on) = 4) = 1/4
+
         2/3 x 1 x 1/3 x 1/2 x 1/3 x 3/4 x 2/3 x 1/3 x 2/4 x 1/3 x 1/3 x 1 x 1/3 x 1 x 1/3 x 1/4 x 1 x 1/2 x 1/4
         """
         expected = {('#start#', 'there'): 2 / 3, ('there', 'is'): 1 / 1, ('is', 'a'): 1 / 3, ('a', 'potato'): 1 / 2,
@@ -106,26 +107,8 @@ class TestMain(TestCase):
     def test_compute_probabilities_per_word_use_Lap_smooth(self):
         """
         'There is a potato on my foot. There is foot on my potato. The potato that is on my foot has a foot on it.'
-        (sum(there|#start#) = 2 / sum(#start#) = 3) = 2/3
-        (sum(is|there) = 2 / sum(there) = 2) = 1
-        (sum(a|is) = 1 / sum(is) = 3) = 1/3
-        (sum(potato|a) = 1 / sum(a) = 2) = 1/2
-        (sum(on|potato) = 1 / sum(potato) = 3) = 1/3
-        (sum(my|on) = 3 / sum(on) = 4) = 3/4
-        (sum(foot|my) = 2 / sum(my) = 3) = 2/3
-        (sum(foot|is) = 1 / sum(is) = 3) = 1/3
-        (sum(on|foot) = 2 / sum(foot) = 4) = 2/4
-        (sum(potato|my) = 1 / sum(potato) = 3) = 1/3
-        (sum(the|#start#) = 1 / sum(#start#) = 3) = 1/3
-        (sum(potato|the) = 1 / sum(the) = 1) = 1
-        (sum(that|potato) = 1 / sum(potato) = 3) = 1/3
-        (sum(is|that) = 1 / sum(that) = 1) = 1
-        (sum(on|is) = 1 / sum(is) = 3) = 1/3
-        (sum(has|foot) = 1 / sum(foot) = 4) = 1/4
-        (sum(a|has) = 1 / sum(has) = 1) = 1
-        (sum(foot|a) = 1 / sum(a) = 2) = 1/2
-        (sum(it|on) = 1 / sum(on) = 4) = 1/4
-        2/3 x 1 x 1/3 x 1/2 x 1/3 x 3/4 x 2/3 x 1/3 x 2/4 x 1/3 x 1/3 x 1 x 1/3 x 1 x 1/3 x 1/4 x 1 x 1/2 x 1/4
+        Same probabilities as test_compute_probabilities_per_word(), but with 1 added to numerator, and V (number of
+        distinct tokens in train corpus) added to denominator.
         """
         expected = {('#start#', 'there'): 2 / 3, ('there', 'is'): 1 / 1, ('is', 'a'): 1 / 3, ('a', 'potato'): 1 / 2,
                     ('potato', 'on'): 1 / 3, ('on', 'my'): 3 / 4, ('my', 'foot'): 2 / 3, ('is', 'foot'): 1 / 3,
@@ -133,8 +116,7 @@ class TestMain(TestCase):
                     ('potato', 'that'): 1 / 3, ('that', 'is'): 1 / 1, ('is', 'on'): 1 / 3, ('foot', 'has'): 1 / 4,
                     ('has', 'a'): 1 / 1, ('a', 'foot'): 1 / 2, ('on', 'it'): 1 / 4}
 
-
-        actual = self.main.compute_probabilities_per_word(corpus=self.corpus2, n=2)
+        actual = self.main.compute_probabilities_per_word(corpus=self.corpus2, n=2, use_Lap_smooth=True)
         self.maxDiff = None
         self.assertEqual(expected, actual)
 
@@ -161,7 +143,7 @@ class TestMain(TestCase):
 
     def test_compute_perplexity(self):
         expected = 1.2457309396155174
-        actual = self.main.compute_perplexity(corpus=self.corpus4, test_corpus='I like', n=2)
+        actual = self.main.compute_perplexity(train_corpus=self.corpus4, test_corpus='I like', n=2)
         self.assertIsInstance(actual, float)
         self.assertEqual(expected, actual)
 
